6ea295a20a8e1365fc74e6f6432c6e64353cf98b
rogue-sword <himansh12@tutamail.com>
2026-03-12 13:25:15 +0530

    Figured out how to embed images successfully

--- Project/src/main.cpp
+++ Project/src/main.cpp
@@ -1,21 +1,20 @@
+#include "assets.hpp"
 #include <SFML/Graphics.hpp>
-#include <SFML/Graphics/Image.hpp>
 
-int main()
-{
-    sf::RenderWindow window(sf::VideoMode({800, 600}), "Snake");
-    window.setIcon(sf::Image("assets/logo.png"));
+int main() {
+  sf::RenderWindow window(sf::VideoMode({800, 600}), "Snake");
+  window.setIcon({LOGO_WIDTH, LOGO_HEIGHT}, logo);
 
-    while (window.isOpen()) {
-        while (const std::optional event = window.pollEvent()) {
-            if (event->is<sf::Event::Closed>()) {
-                window.close();
-            }
-        }
-
-        window.clear(sf::Color::Cyan);
-
-        window.display();
+  while (window.isOpen()) {
+    while (const std::optional event = window.pollEvent()) {
+      if (event->is<sf::Event::Closed>()) {
+        window.close();
+      }
     }
-    return 0;
+
+    window.clear(sf::Color::Cyan);
+
+    window.display();
+  }
+  return 0;
 }
