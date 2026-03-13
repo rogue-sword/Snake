7c55efb06aadfbcda9e249779f31131357f7906f
rogue-sword <himansh12@tutamail.com>
2026-03-13 19:01:09 +0530

    Work on the snake itself

--- Project/src/main.cpp
+++ Project/src/main.cpp
@@ -1,31 +1,56 @@
-#include "arena.h"
 #include "logo.h"
 #include <SFML/Graphics.hpp>
 
-static const inline sf::Image Icon() {
-  return sf::Image({LOGO_WIDTH, LOGO_HEIGHT}, logo);
-}
+enum dir {UP, DOWN, LEFT, RIGHT};
 
-// In this program it is assumed that even if you change the size of arena it is
-// still of the same size as the window so, you don't need to center or resize
-// it.'
+class SnakeUnit {
+private:
+  dir direction;
+  SnakeUnit* next;
+public:
+  SnakeUnit(dir d, SnakeUnit* n): next(n), direction(d) {}
+  SnakeUnit(dir d): direction(d), next(nullptr) {}
+  SnakeUnit(): direction(dir::UP), next(nullptr) {}
+  unsigned length(SnakeUnit* head) {
+    unsigned length = 0;
+    while (head) {
+      length += 1;
+      head = head->next;
+    }
+    return length;
+  };
+  void extend(SnakeUnit* tail) {
+    tail->next = new SnakeUnit;
+    tail->next->next = nullptr;
+    tail->next->direction = tail->direction;
+  }
+};
+
 int main() {
-  sf::RenderWindow window(sf::VideoMode({800, 600}), "Snake");
-  window.setIcon(Icon());
+  // Spare me the complexity and just keep the window the same size
+  sf::RenderWindow window(sf::VideoMode({800, 600}), "Snake", sf::Style::Close);
+  window.setIcon(sf::Image({LOGO_WIDTH, LOGO_HEIGHT}, logo));
 
-  sf::Texture _board(sf::Image({ARENA_WIDTH, ARENA_HEIGHT}, arena));
-  sf::Sprite board(_board);
+  constexpr float borderWidth{50};
+
+  auto worldBorder = window.getSize();
 
   while (window.isOpen()) {
     while (const std::optional event = window.pollEvent()) {
       if (event->is<sf::Event::Closed>()) {
         window.close();
+      } else if (event->is<sf::Event::Resized>()) {
+        worldBorder = window.getSize();
       }
     }
+    sf::RectangleShape playingArea({(float)worldBorder.x - borderWidth,
+                                    (float)worldBorder.y - borderWidth});
+    playingArea.setFillColor(sf::Color::Blue);
+    playingArea.setPosition({borderWidth / 2, borderWidth / 2});
 
     window.clear(sf::Color::Cyan);
 
-    window.draw(board);
+    window.draw(playingArea);
 
     window.display();
   }
