809e717d757c8527992e80d17c183bb51390465a
rogue-sword <himansh12@tutamail.com>
2026-03-13 17:52:50 +0530

    Made some progress with the images

--- Project/src/main.cpp
+++ Project/src/main.cpp
@@ -1,9 +1,20 @@
-#include "assets.hpp"
+#include "arena.h"
+#include "logo.h"
 #include <SFML/Graphics.hpp>
 
+static const inline sf::Image Icon() {
+  return sf::Image({LOGO_WIDTH, LOGO_HEIGHT}, logo);
+}
+
+// In this program it is assumed that even if you change the size of arena it is
+// still of the same size as the window so, you don't need to center or resize
+// it.'
 int main() {
   sf::RenderWindow window(sf::VideoMode({800, 600}), "Snake");
-  window.setIcon({LOGO_WIDTH, LOGO_HEIGHT}, logo);
+  window.setIcon(Icon());
+
+  sf::Texture _board(sf::Image({ARENA_WIDTH, ARENA_HEIGHT}, arena));
+  sf::Sprite board(_board);
 
   while (window.isOpen()) {
     while (const std::optional event = window.pollEvent()) {
@@ -13,6 +24,8 @@ int main() {
     }
 
     window.clear(sf::Color::Cyan);
+
+    window.draw(board);
 
     window.display();
   }
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Snake
 A Snake game made using C++ &amp; SFML.
+
+### Information
+- The actual code is inside the project folder.
+- You will need: git cmake ninja.
+- You will also need clang-format if you wish to use the format option provided in the makefile.
+- The images are embeded in the code for creating standalone binary.
+- The image arena was created by me &amp; logo was taken from svg-repo and is present under the CC0 License.
